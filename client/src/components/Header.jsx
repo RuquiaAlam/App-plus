ea31170e94238c22e3fec792e31e23ffd04663a0
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-04-15 16:58:30 -0500

    complete signup page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,10 @@
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="bg-slate-200">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
-        <h1 className="text-xl text-blue-600 font-bold">App Plus</h1>
+        <h1 className="text-xl my-3 text-blue-600 font-bold">App Plus</h1>
 
         <ul className="flex gap-4">
           <Link to="/">
@@ -14,7 +13,7 @@ export default function Header() {
           <Link to="/about">
             <li>About</li>
           </Link>
-        
+
           <Link to="/sign-in">
             <li>SignIn</li>
           </Link>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  // const {username,email,password}=
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg  mx-auto items-center">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form className="flex flex-col gap-3 ">
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-gray-200 p-3 rounded-lg"
+        ></input>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-gray-200 p-3 rounded-lg"
+        ></input>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-gray-200 p-3 rounded-lg"
+        ></input>
+        <button className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-95">
+          Sign Up
+        </button>
+        <button className="bg-red-500 p-3 rounded-lg text-white">
+          Sign Up with Google
+        </button>
+        <div className="flex gap-2 mt-5">
+          <p>Have an account?</p>
+          <Link to="/sign-in">
+            <span className="text-blue-500">Sign in</span>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
