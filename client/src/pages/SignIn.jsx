e1a12921c5b5a103df473fe6efa1c0cf8ccf3074
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-04-15 21:20:06 -0500

    complete the signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,16 +1,47 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+
+      setError(false);
+    } catch (err) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   return (
     <div className="p-3 max-w-lg  mx-auto items-center">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form className="flex flex-col gap-3 ">
+      <form className="flex flex-col gap-3 " onSubmit={handleSubmit}>
         <input
           type="text"
           id="email"
@@ -25,16 +56,20 @@ export default function SignIn() {
           className="bg-gray-200 p-3 rounded-lg "
           onChange={handleChange}
         ></input>
-        <button className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-95">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-95"
+        >
+          {loading ? "Loading..." : "Sign In"}
         </button>
 
         <div className="flex gap-2 mt-5">
-          <p> Don't have an account?</p>
+          <p> Dont have an account?</p>
           <Link to="/sign-up">
             <span className="text-blue-500">Sign up</span>
           </Link>
         </div>
+        <p>{error && "Something went wrong"}</p>
       </form>
     </div>
   );
