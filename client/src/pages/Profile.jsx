ddc6a972bb7d20701271c968e47dc926d13725f1
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-04-19 15:41:41 -0500

    add update user and delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,16 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+} from "../redux/user/userSlice.js";
+
 export default function Profile() {
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
@@ -18,6 +28,7 @@ export default function Profile() {
   const [updateSuccess, setUpdateSuccess] = useState(false);
 
   const { currentUser, loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   //firebase storage rules
   //   service firebase.storage {
   //   match /b/{bucket}/o {
@@ -43,7 +54,7 @@ export default function Profile() {
       (snapshot) => {
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(progress);
+        console.log(progress);
         setImagePercent(Math.round(progress));
         console.log(imagePercent);
       },
@@ -57,10 +68,53 @@ export default function Profile() {
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  // console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        header: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+      console.log(data);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+    }
+  };
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  };
   return (
     <div className="p-3 max-w-lg  mx-auto ">
       <h1 className="text-3xl  text-center font-semibold my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
           ref={fileRef}
@@ -69,7 +123,7 @@ export default function Profile() {
           onChange={(e) => setImage(e.target.files[0])}
         />
         <img
-          src={formData.profielPicture||currentUser.profielPicture}
+          src={formData.profielPicture || currentUser.profielPicture}
           alt="profilepic"
           className="h-24 w-24  mt-2 self-center cursor-pointer rounded-full  object-cover"
           onClick={() => fileRef.current.click()}
@@ -77,7 +131,6 @@ export default function Profile() {
         <p className="text-sm self-center">
           {imageError ? (
             <span className="text-red-700">
-           
               Error uploading image (file size must be less than 2 MB)
             </span>
           ) : imagePercent > 0 && imagePercent < 100 ? (
@@ -95,6 +148,7 @@ export default function Profile() {
           id="name"
           placeholder="Username"
           className=" bg-gray-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           defaultValue={currentUser.email}
@@ -102,20 +156,28 @@ export default function Profile() {
           id="email"
           placeholder="email"
           className=" bg-gray-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="text"
           id="password"
           placeholder="password"
           className=" bg-gray-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <button className="uppercase bg-slate-700 text-white rounded-lg py-2 hover:opacity-95 disabled:opacity-80">
-          Update
+          {loading ? "Loading..." : "Update"}
         </button>
         <div className="flex justify-between mt-5">
-          <span className="text-red-500">Delete Account</span>
+          <span onClick={handleDeleteAccount} className="text-red-500 cursor-pointer">
+            Delete Account
+          </span>
           <span className="text-red-500">Sign out</span>
         </div>
+        <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+        <p className="text-green-700 mt-5">
+          {updateSuccess && "User updated successfully!"}
+        </p>
       </form>
     </div>
   );
