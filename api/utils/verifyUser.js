9a628fc930acdf269a26398d8742c2867e5bcacb
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2024-04-19 13:54:01 -0500

    create update user api route

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_Token;
+  if (!token) {
+    return next(errorHandler(401, "You are not authenticated!"));
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Token Invalid!"));
+    req.user = user;
+    next();
+  });
+};
